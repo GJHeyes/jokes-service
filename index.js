7b633aad0b587145b51ff320d5b702e4c0dd405e
GJHeyes <gregory-heyes@hotmail.com>
2023-01-12 10:18:13 +0000

    first commit

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const { Joke } = require('./db');
+const { Joke } = require("./db");
+const { Op } = require("sequelize");
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/jokes', async (req, res, next) => {
+app.get("/jokes", async (req, res, next) => {
+  let jokes = [];
   try {
+    if (req.query.tags) {
+      jokes = await Joke.findAll({
+        where: {
+          tags: {
+            [Op.like]: "%" + req.query.tags + "%",
+          },
+        },
+      });
+    } else if (req.query.content) {
+      jokes = await Joke.findAll({
+        where: {
+          joke: {
+            [Op.like]: "%" + req.query.content + "%",
+          },
+        },
+      });
+    } else {
+      jokes = await Joke.findAll();
+    }
     // TODO - filter the jokes by tags and content
-    const jokes = [];
     res.send(jokes);
   } catch (error) {
     console.error(error);
-    next(error)
+    next(error);
   }
 });
 
